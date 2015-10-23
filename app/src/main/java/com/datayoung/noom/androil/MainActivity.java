b701f31a3bc93f1b96713cd72dbe8ec9a89bfc4b
noomdatayoung <noom@datayoung.com>
2015-10-23 09:10:52 +0700

    no full screen
    orginal

--- app/src/main/java/com/datayoung/noom/androil/MainActivity.java
+++ app/src/main/java/com/datayoung/noom/androil/MainActivity.java
@@ -2,7 +2,6 @@ package com.datayoung.noom.androil;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +9,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
